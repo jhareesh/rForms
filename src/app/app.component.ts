ff351c408532d09a60719269c714bdc6340c30c4
FullStackDeveloper-j <128317139+FullStackDeveloper-j@users.noreply.github.com>
2023-05-02 17:21:58 +0530

    created reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { FormGroup,FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rForms';
+  
+  //ACHIEVING TWO WAY INTERACTIONu
+  form = new FormGroup({
+    name:new FormControl(),
+    phone : new FormControl(),
+    email : new FormControl()
+  })
+
+  //defining the event
+  signup()
+  {
+    console.log(this.form.value)//passing the reference of FormGroup object
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+//importing reactive forms module
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // importing forms module to use along with the bootsrap
+    FormsModule,
+    // importing reactiveFormsModule to achieve Reactive Forms
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
